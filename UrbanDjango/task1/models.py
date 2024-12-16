54d2e251943aa4961d0e76b2b5a5c7baf1252ea1
Taerrean <terrar1umand@gmail.com>
2024-12-16 16:23:46 +0300

    Add files via upload

--- UrbanDjango/task1/models.py
+++ UrbanDjango/task1/models.py
@@ -19,3 +19,18 @@ class Game(models.Model):
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer, related_name='games')
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    amount = models.IntegerField()
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    customer = models.ManyToManyField(Customer, related_name='products')
